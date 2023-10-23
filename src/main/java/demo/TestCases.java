214951d676591ca13cd79ccd28a44137acb01cc9
varadkharade <varadkharade94@gmail.com>
2023-10-23 10:25:08 +0530

    Initalized Project

---
+++ src/main/java/demo/TestCases.java
@@ -0,0 +1,36 @@
+package demo;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class TestCases {
+    ChromeDriver driver;
+    public TestCases()
+    {
+        System.out.println("Constructor: TestCases");
+        WebDriverManager.chromedriver().timeout(30).setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    }
+
+    public void endTest()
+    {
+        System.out.println("End Test: TestCases");
+        driver.close();
+        driver.quit();
+
+    }
+
+    
+    public  void testCase01(){
+        System.out.println("Start Test case: testCase01");
+        driver.get("https://www.google.com");
+        System.out.println("end Test case: testCase02");
+    }
+
+
+}
